c998d1cef2e6443654d969630d0709ec434effcc
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2022-10-05 01:31:37 +0530

    text preprocessing updated

--- build_model/preprocessing.py
+++ build_model/preprocessing.py
@@ -1,5 +1,9 @@
+from lib2to3.pgen2 import token
 import re
 import string
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
 
 
 def cleantext(text_str: str) -> str:
@@ -13,6 +17,18 @@ def cleantext(text_str: str) -> str:
     text_str=' '.join(text_str.split())
     return text_str
 
+def tokenizeit(text_str: str) -> list:
+    return word_tokenize(text_str)
+
+def lemmatizeit(text_str: str) -> str:
+    wnl = WordNetLemmatizer()
+    return wnl.lemmatize(text_str)
+
 if __name__ == "__main__":
-    txt = "Hello12 My name is      Admiral! General! Aaladeen :)"
-    print(cleantext(txt))
+    txt = "Hello12 My name is      Admiral! General! Aaladeen:)"
+    clntxt = cleantext(txt)
+    print(clntxt)
+    tokentxt = tokenizeit(clntxt)
+    print(tokentxt)
+    lemtxt = [lemmatizeit(x) for x in tokentxt]
+    print(lemtxt)
