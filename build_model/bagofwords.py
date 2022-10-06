199a310de7e243a898b4b72409b4e6368551cfc4
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2022-10-06 23:57:53 +0530

    completed bag of words algorithm

--- build_model/bagofwords.py
+++ build_model/bagofwords.py
@@ -1,31 +1,45 @@
-import csv
 from preprocessing import cleantext
 from tqdm import tqdm
 import os
+import time
 
 
-def _unquiwords(corpus:list) -> dict:
+def _unquiwords(corpus:list,method:str='count') -> dict:
+    """
+    return a dictionary of all words in the corpus and their
+    numeric weight according to the defined method
+    args:
+    corpus = list of documents
+    method = count | frequency
+    """
     d_wordcount = {}
+    total_words = 0
     for row in tqdm(corpus):
-        for word in row[1]: # row[0] is label, row[1] is tweet
+        word_list = row.split(" ")
+        total_words += len(word_list)
+        for word in word_list: # row[0] is label, row[1] is tweet
             if word not in d_wordcount.keys():
                 d_wordcount[word] = 1
             d_wordcount[word] += 1
-
+    if method == 'frequency':
+        for k,v in d_wordcount.items():
+            d_wordcount[k] = round(v/total_words,4)
     return d_wordcount
 
 def _doc_to_vector(corpus: list, wordcount: dict) -> list:
+    max_vec_len = 0
     output_vector_corpus = os.path.join('data','output_vector.txt')
     with open(output_vector_corpus,'w') as f:
         for idx,row in tqdm(enumerate(corpus)):
             if idx == 0: continue
             doc_text_vector = row[1].split(" ")
             doc_vector = [wordcount.get(x,0) for x in doc_text_vector]
-            doc_vector_str = ",".join(doc_vector)
-            print("row vector is: ",doc_vector_str)
-            f.write(f"{row[0]},{','.join(doc_vector)}\n")
+            if len(doc_vector) > max_vec_len:
+                max_vec_len = len(doc_vector)
+            doc_vector_str = ",".join([str(x) for x in doc_vector])
+            f.write(f"{row[0]},{doc_vector_str}\n")
     
-    print("Length of document vector is:", len(doc_vector))
+    print("Max length of document vector is:", max_vec_len)
 
 
 
@@ -42,10 +56,13 @@ def _file_to_corpus(fpath: str) -> list:
     return li_tuples
 
 if __name__ == '__main__':
+    start_time = time.time()
     csv_path = os.path.join('data','train.csv')
     li_tuples = _file_to_corpus(csv_path)
-    wordcount = _unquiwords(li_tuples)
+    corpus_list = [x[1] for x in li_tuples]
+    wordcount = _unquiwords(corpus_list, 'frequency')
     _doc_to_vector(li_tuples, wordcount)
+    print(f"Total run time: {time.time() - start_time}s")
 
 
         
--- build_model/preprocessing.py
+++ build_model/preprocessing.py
@@ -11,9 +11,10 @@ def cleantext(text_str: str) -> str:
     text_str = text_str.translate(str.maketrans(' ',' ',string.punctuation)) # remove punctuations
     text_str = re.sub('[^a-zA-Z]',' ',text_str) # Only alphabets
     text_str = re.sub("\n"," ",text_str)
+    text_str = re.sub(' +', ' ', text_str) # remove multiple spaces
     text_str = text_str.lower()
     # will split and join the words
-    text_str=' '.join(text_str.split())
+    text_str = ' '.join(text_str.split())
     return text_str
 
 def tokenizeit(text_str: str) -> list:
