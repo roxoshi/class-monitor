7efd823e7ad147a320fc5abbf85c091348f4bc85
rojo-jojo <40956690+rojo-jojo@users.noreply.github.com>
2022-10-08 18:20:34 +0530

    refactoring

--- build_model/bagofwords.py
+++ build_model/bagofwords.py
@@ -4,19 +4,34 @@ import os
 import time
 
 
-def _unquiwords(corpus:list,method:str='count') -> dict:
+
+def _file_to_corpus(fpath: str) -> list:
+    li_tuples = []
+    with open(fpath, 'r') as f:
+        raw_txt = f.readlines()
+    for row in raw_txt:
+        row_list = row.split(",", maxsplit=3)
+        li_tuples.append((row_list[1],row_list[2])) # tuple of (label, tweet)
+    
+    #print("Sample corpus tuple:\n",li_tuples)
+    return li_tuples
+
+def clean_tuple(tuple_list):
+    return [(x[0],cleantext(x[1])) for x in tuple_list]
+
+def word_freq_count(corpus:list,method:str='count') -> dict:
     """
     return a dictionary of all words in the corpus and their
     numeric weight according to the defined method
     args:
-    corpus = list of documents
+    corpus = list of tuples with label and documents
     method = count | frequency
     """
     #corpus = [x[1] for x in corpus]
     d_wordcount = {}
     total_words = 0
     for row in tqdm(corpus):
-        word_list = row.split(" ")
+        word_list = row[1].split(" ")
         total_words += len(word_list)
         for word in word_list: # row[0] is label, row[1] is tweet
             if word not in d_wordcount.keys():
@@ -27,8 +42,13 @@ def _unquiwords(corpus:list,method:str='count') -> dict:
             d_wordcount[k] = round(v/total_words,4)
     return d_wordcount
 
-def _doc_to_vector(corpus: list) -> str:
-    wordcount = _unquiwords(corpus)
+def document_to_vector(corpus: list) -> str:
+    """
+    Input: list of tuple like [(label, tweet),..,..,]
+    Output: Returns path of csv with label as first column
+        and feature vector
+    """
+    wordcount = word_freq_count(corpus)
     max_vec_len = 0
     output_vector_corpus = os.path.join('data','output_vector.txt')
     with open(output_vector_corpus,'w') as f:
@@ -44,30 +64,13 @@ def _doc_to_vector(corpus: list) -> str:
     print("Max length of document vector is:", max_vec_len)
     return output_vector_corpus
 
-
-
-def _file_to_corpus(fpath: str) -> list:
-    li_tuples = []
-    with open(fpath, 'r') as f:
-        raw_txt = f.readlines()
-    for row in raw_txt:
-        row_list = row.split(",", maxsplit=3)
-        li_tuples.append((row_list[1],row_list[2])) # tuple of (label, tweet)
-    
-    #print("Sample corpus tuple:\n",li_tuples)
-    
-    return li_tuples
-
-def clean_tuple(tuple_list):
-    return [cleantext(x[1]) for x in tuple_list]
-
-if __name__ == '__main__':
+def main():
     csv_path = os.path.join('data','train.csv')
     inp = csv_path
-    for func in [_file_to_corpus, clean_tuple, _doc_to_vector]:
+    for func in [_file_to_corpus, clean_tuple, document_to_vector]:
         inp = func(inp)
     
-    print("final output is: ", inp)
+    return inp
 
 
         
